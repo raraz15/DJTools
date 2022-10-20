d4092d2b055a44e2f314f3d7640bc93e6f21bed7
Recep Oguz Araz <oguza97@gmail.com>
2022-10-20 18:29:24 +0200

    better print

--- byte.py
+++ byte.py
@@ -1,10 +1,11 @@
 import os
+import sys
 from glob import glob
 import time
 import argparse
 
 import warnings
-warnings.filterwarnings("error")
+#warnings.filterwarnings("error")
 
 import librosa
 
@@ -17,15 +18,18 @@ if __name__=="__main__":
     file_paths=sorted([path for path in glob(f"{args.path}/*.mp3")])
     total_time=0
     for file_path in file_paths:
-        start_time=time.time()
+        print("="*100)
         print(f"Analyzing: {os.path.basename(file_path)}")
+        start_time=time.time()
         try:
             sr=librosa.get_samplerate(file_path)
             y,_=librosa.load(file_path,sr=sr)
-            print(f"{len(y)} samples returned.")
-        except: #Warning
-            print("Unkown Error!")
+            if len(y)==0:
+                print("0 samples returned.")
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            print("Unexpected Error!")
         end_time=time.time()
         total_time+=end_time-start_time
-        print("="*100)
     print(f"{total_time:.2f} seconds total processing")
