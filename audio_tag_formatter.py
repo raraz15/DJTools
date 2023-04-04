6f47a0020141e4785a4d644d0c94990b694e544c
Recep Oguz Araz <oguza97@gmail.com>
2023-04-04 21:13:18 +0200

    accept single files

--- audio_tag_formatter.py
+++ audio_tag_formatter.py
@@ -7,34 +7,40 @@ from tag_formating import mp3_tag_formatter,flac_tag_formatter,wav_tag_formatter
 
 EXT=[".mp3",".flac",".wav"]
 
+def main(file_path):
+    # File name cleaning
+    file_path=clean_file_path(file_path)
+    ext=os.path.splitext(file_path)[1]
+    if ext==".mp3":
+        mp3_tag_formatter(file_path)
+    elif ext==".wav":
+        wav_tag_formatter(file_path)
+    elif ext==".flac":
+        flac_tag_formatter(file_path)
+    else:
+        print("Unsupported file type!")
+
 if __name__=="__main__":
 
     parser=argparse.ArgumentParser()
-    parser.add_argument("-p","--path",type=str,required=True,help="Path to directory containing audio files.")
+    parser.add_argument("-p", "--path", type=str, required=True,
+                        help="Path to audio file or directory containing audio files.")
     args=parser.parse_args()
 
-    # Find the audio file paths
-    file_paths=sorted([path for ext in EXT for path in glob(f"{args.path}/*{ext}")])
-    print(f"{len(file_paths)} tracks found in:\n{args.path}")
-
-    # Clean all the files
-    print("Starting the cleanup process...")
-    try:
-        for i,file_path in enumerate(file_paths):
-            print("="*80)
-            print(f"[{i+1}/{len(file_paths)}]")
-            # File name cleaning
-            file_path=clean_file_path(file_path)
-            ext=os.path.splitext(file_path)[1]
-            if ext==".mp3":
-                mp3_tag_formatter(file_path)
-            elif ext==".wav":
-                wav_tag_formatter(file_path)
-            elif ext==".flac":
-                flac_tag_formatter(file_path)
-            else:
-                print("Unsupported file type!")
-    except KeyboardInterrupt:
-        sys.exit()
-    print("="*80)
+    if os.path.isfile(args.path):
+        main(args.path)
+    else:
+        # Find the audio file paths
+        file_paths=sorted([path for ext in EXT for path in glob(f"{args.path}/*{ext}")])
+        print(f"{len(file_paths)} tracks found in:\n{args.path}")
+        # Clean all the files
+        print("Starting the cleanup process...")
+        try:
+            for i,file_path in enumerate(file_paths):
+                print("="*80)
+                print(f"[{i+1}/{len(file_paths)}]")
+                main(file_path)
+        except KeyboardInterrupt:
+            sys.exit()
+        print("="*80)
     print("Done!")
